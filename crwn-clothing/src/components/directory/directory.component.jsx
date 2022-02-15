c6e224af2b13150714dc608505539cf1bae4f932
pardha saradhi chodey <pardhu.dhoni@gmail.com>
2022-02-15 16:59:24 +0530

    Header

---
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+import SECTIONS_DATA from './sections.data';
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: SECTIONS_DATA,
+    };
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
